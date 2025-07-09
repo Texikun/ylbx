0f02f4fe345e4c98e8dc624d3e3f69ae26bd36b5
Texikun <1728260357@qq.com>
2025-07-09 08:52:00 +0800

    dad

---
+++ src/main/java/com/neuedu/ylzx/YlzxApplication.java
@@ -0,0 +1,13 @@
+package com.neuedu.ylzx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YlzxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YlzxApplication.class, args);
+    }
+
+}
